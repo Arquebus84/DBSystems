7cef61815657dd8f47596deea30d1afa72370632
Arquebus84 <matteoventura06@gmail.com>
2026-04-20 22:39:26 -0400

    Fixed Button Functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask ,render_template, url_for, request
+from flask import Flask ,render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
@@ -16,48 +16,54 @@ class DatabaseTest(db.Model):
 
     def __repr__(self):
         return '<Task %r>' % self.id
-    
-# @app.route('/', methods=['POST', 'GET'])
-# def index():
-#     if(request.method == 'POST'):
-#         # Grab the task and place it into the DB
-#         return "Hello World"
-#     else:
-#         # Otherwise, just view the page
-#         return render_template('base.html')
-    
-# @app.route('/patient-contents', methods=['PATIENT', 'FAMILY', 'ROOM', 'PAYMENT'])
-# def patientIndex():
-#     if(request.method == 'PATIENT'):
-#         return 'access patient methods'
-#     else:
-#         return render_template('base.html')
-    
 
-# # @app.route('/familyTable')
-# def facultyTable():
-#     pass
-# # @app.route('/patientRoomTable')
-# def facultyTable():
-#     pass
-# # @app.route('/paymentSummaryTable')
-# def facultyTable():
-#     pass
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('base.html')
 
+@app.route('/content', methods=['POST', 'GET'])
+def tableIndex():
+    if(request.method == 'POST'):
+        # Grab the task and place it into the DB
+        return "Hello World"
+    else:
+        # Otherwise, just view the page
+        return render_template('base.html')
 
-# # @app.route('/facultyTable')
-# def facultyTable():
-#     pass
-# # @app.route('/assignmentTable')
-# def facultyTable():
-#     pass
+@app.route('/home/patient-options', methods=['POST'])
+def patient_options():
+    pressed = request.form.get('bt')
+    if(pressed == 'patient-table'):
+        return render_template('patient.html')
+    elif(pressed == 'patient-family'):
+        return render_template('family.html')
+    elif(pressed == 'patient-rooms'):
+        return render_template('room.html')
+    elif(pressed == 'patient-payment'):
+        return render_template('summary.html')
+    else:
+        return render_template('base.html')
 
-# # @app.route('/medicationTable')
-# def facultyTable():
-#     pass
-# # @app.route('/patientMedTable')
-# def facultyTable():
-#     pass
+@app.route('/home/faculty-options', methods=['POST'])
+def faculty_options():
+    pressed = request.form.get('bt')
+    if(pressed == 'faculty-table'):
+        return render_template('faculty.html')
+    elif(pressed == 'faculty-assign'):
+        return render_template('assignment.html')
+    else:
+        return render_template('base.html')
+
+@app.route('/home/medication-options', methods=['POST'])
+def medication_options():
+    pressed = request.form.get('bt')
+    if(pressed == 'medication-table'):
+        return render_template('medication.html')
+    elif(pressed == 'medication-patient'):
+        return render_template('patientMed.html')
+    else:
+        return render_template('base.html')
 
 if(__name__ == '__main__'):
     app.run(debug=True)
