064c8f44b06474a21869fa7b72e5eda063bb0e3c
Arquebus84 <matteoventura06@gmail.com>
2026-04-21 03:44:03 -0400

    Initial Commit

--- main.py
+++ main.py
@@ -1,47 +1,137 @@
 from flask import Flask ,render_template, url_for, request, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 from flask_sqlalchemy import SQLAlchemy 
 
+import mysql.connector
+from flask import Flask
+
 app = Flask(__name__)
-# Note: using two // will indicate the use of a driver on this computer
-# This is especially important if used 'pip install mysql-connector-python' command
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:ithertzwhenIP#1984@localhost/nursingHomeDB.sql'
 
-db = SQLAlchemy(app)
+try:
+    db = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='ithertzwhenIP#1984',
+        database='nursingHomeDB'
+    )
 
-class DatabaseTest(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    # date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    cursor = db.cursor()
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
+except mysql.connector.Error as e:
+    medications = ["#"]
+    print(e)
+    
+def getPatientTable():
+    return 'SELECT p.patientID AS ID, p.firstName AS firstName, p.lastName AS lastName, p.patientPriority AS priority, ' \
+    'p.conditionDesc AS conditionDesc, tf.familyLastName AS familyContact ' \
+    'FROM patient p JOIN trusted_family tf ' \
+    'ON p.familyID = tf.familyID'
+def getFamilyTable():
+    return 'SELECT tf.familyLastName AS familyName, pn.phoneNumber AS phoneNumber ' \
+    'FROM trusted_family tf JOIN phone_number pn WHERE tf.phoneNumberID = pn.numberID'
+def getRoomTable():
+    return 'SELECT r.patientRoomNumber AS roomNumber, p.firstName AS firstName, p.lastName AS lastName ' \
+    'FROM patient_room r JOIN patient p WHERE r.patientID = p.patientID'
+def getPaymentTable():
+    return 'SELECT ROUND(sum(sys.price * sys.tax * 0.01 + sys.price), 2) AS netPayment, p.firstName AS firstName, p.lastName AS lastName ' \
+    'FROM payment_summary psum JOIN patient p JOIN payment_system sys ' \
+    'WHERE p.patientID = psum.patientID AND psum.paymentID = sys.paymentID GROUP BY p.firstName, p.lastName'
+
+def getFacultyTable():
+    return 'SELECT f.facultyID, f.facultyLastName, ft.facultyType ' \
+    'FROM faculty f JOIN faculty_type ft ON f.facultyTypeID = ft.facultyTypeID'
+def getAssignTable():
+    return 'select f.facultyLastName AS facultyName, p.firstName AS firstName, p.lastName AS lastName, pr.patientRoomNumber AS roomNum, ' \
+    'case ' \
+    'when (pr.patientRoomNumber >= 3000) then ifnull(ar.floorNumber, 3) ' \
+    'when (pr.patientRoomNumber >= 2000) then ifnull(ar.floorNumber, 2) ' \
+    'else ifnull(ar.floorNumber, 1) ' \
+    'end AS FloorNumber from assigned_room ar ' \
+    'join patient_room pr join patient p join faculty f ' \
+    'where ar.patientRoomID = pr.patientRoomID AND ar.facultyID = f.facultyID AND pr.patientID = p.patientID'
+
+def getMedicationTable():
+    return 'SELECT m.medicationType, p.price, p.tax FROM medication m JOIN payment_system p WHERE p.paymentID = m.paymentID'
+def getPatientMedTable():
+    return 'SELECT p.firstName AS firstName, p.lastName AS lastName, med.medicationType AS medication ' \
+            'FROM patient_med pmed '\
+            'JOIN medication med '\
+            'JOIN patient p '\
+            'WHERE pmed.patientID = p.patientID AND pmed.medicationID = med.medicationID'
+
+
+#     _medication = StringField("Add Medication")
+#     _price = StringField("Add Price")
+#     _tax = StringField("Add Tax")
+#     _submit = SubmitField("Submit")
 
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template('base.html')
 
-@app.route('/content', methods=['POST', 'GET'])
-def tableIndex():
-    if(request.method == 'POST'):
-        # Grab the task and place it into the DB
-        return "Hello World"
-    else:
-        # Otherwise, just view the page
-        return render_template('base.html')
+@app.route('/home/add', methods=['POST', 'GET'])
+def addMedication():
+    return render_template('medication.html', medications=medications)
+
+    # medType = None
+    # form = MedForm()                            #The form in render_template will be equal to this form in this file
+    # if(form.validate_on_submit()):
+    #     med = Medication.query.filter_by(_medication=form._medication.data).first()
+    #     if(med is None):
+    #         med = Medication(_medication=form._medication.data)
+    #         db.session.add(med)
+    #         db.session.commit()
+    #     medType = form._medication.data
+    #     form._medication.data=''
+    #     form._price.data=''
+    #     form._tax.data=''
+    #     print("Medication Added")
+    # medications = Medication.query.group_by(Medication.medType)
+    # return render_template('medication.html', 
+    #                        form=form,
+    #                        medType=medType,
+    #                        medications=medications)
+
+# def tableIndex():
+#     if(request.method == 'POST'):
+#         # Grab the data
+#         dbContent = request.form['medication']  # Request the name of the row in medication.html
+#         newMed = DatabaseTest(med = dbContent)
+
+#         #Push data into the DB
+#         try:
+#             db.session.add(newMed)
+#             db.session.commit()
+#             return redirect('/')
+#         except Exception as e:
+#             print(e)
+#             return 'Issue found when adding new data'
+#     else:
+#         # Otherwise, just view the page
+#         newRow = DatabaseTest.query.order_by(DatabaseTest.id).all()
+#         return render_template('medication.html', medication=newRow)
 
 @app.route('/home/patient-options', methods=['POST'])
 def patient_options():
     pressed = request.form.get('bt')
     if(pressed == 'patient-table'):
-        return render_template('patient.html')
+        cursor.execute(getPatientTable())
+        patients = cursor.fetchall()
+        return render_template('patient.html', patients=patients)
     elif(pressed == 'patient-family'):
-        return render_template('family.html')
+        cursor.execute(getFamilyTable())
+        family = cursor.fetchall()
+        return render_template('family.html', family=family)
     elif(pressed == 'patient-rooms'):
-        return render_template('room.html')
+        cursor.execute(getRoomTable())
+        room = cursor.fetchall()
+        return render_template('room.html', room=room)
     elif(pressed == 'patient-payment'):
-        return render_template('summary.html')
+        cursor.execute(getPaymentTable())
+        summary = cursor.fetchall()
+        return render_template('summary.html', summary=summary)
     else:
         return render_template('base.html')
 
@@ -49,9 +139,13 @@ def patient_options():
 def faculty_options():
     pressed = request.form.get('bt')
     if(pressed == 'faculty-table'):
-        return render_template('faculty.html')
+        cursor.execute(getFacultyTable())
+        faculty = cursor.fetchall()
+        return render_template('faculty.html', faculty=faculty)
     elif(pressed == 'faculty-assign'):
-        return render_template('assignment.html')
+        cursor.execute(getAssignTable())
+        assign = cursor.fetchall()
+        return render_template('assignment.html', assign=assign)
     else:
         return render_template('base.html')
 
@@ -59,9 +153,15 @@ def faculty_options():
 def medication_options():
     pressed = request.form.get('bt')
     if(pressed == 'medication-table'):
-        return render_template('medication.html')
+        cursor.execute(getMedicationTable())
+        medications = cursor.fetchall()
+        return render_template('medication.html', medications=medications)
+    
     elif(pressed == 'medication-patient'):
-        return render_template('patientMed.html')
+        cursor.execute(getPatientMedTable())
+        patient_medications = cursor.fetchall()
+        return render_template('patientMed.html', patient_medications=patient_medications)
+    
     else:
         return render_template('base.html')
 
--- model/database.py
+++ model/database.py
@@ -1,14 +1,34 @@
 import mysql.connector
+from flask import Flask
 
-db = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='ithertzwhenIP#1984',
-    database='nursingHomeDB'
-)
+app = Flask(__name__)
 
-cursor = db.cursor()
-cursor.execute("SELECT f.facultyLastName, ft.facultyType FROM faculty f JOIN faculty_type ft WHERE f.facultyTypeID = ft.facultyTypeID")
+def check_connection():
+    try:
+        db = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='ithertzwhenIP#1984',
+            database='nursingHomeDB'
+        )
+        return True
+    except mysql.connector.Error as e:
+        print(e)
+        return False
 
-for x in cursor:
-    print(x)
+@app.route('/')
+def index():
+    is_connected = check_connection()
+    if(is_connected):
+        return '<h4>Connected Successfully<h4>'
+    else:
+        return '<h4>Not Connected<h4>'
+    
+# if(__name__ == '__main__'):
+#     app.run(debug=True)
+
+# cursor = db.cursor()
+# cursor.execute("SELECT f.facultyLastName, ft.facultyType FROM faculty f JOIN faculty_type ft WHERE f.facultyTypeID = ft.facultyTypeID")
+
+# for x in cursor:
+#     print(x)
